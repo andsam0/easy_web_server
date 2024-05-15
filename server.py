d01d2262d0f9d847a136f0a055a2aa9988a637ce
Andrea Samori <andrea.samori3@studio.unibo.it>
2024-05-15 12:09:45 +0200

    added ctrl-c handler and changed some names

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 #!/bin/env python
 from threading import Thread
 from socket import * 
-
+import sys, signal
 def accetta_richieste():
     while True:
-        client, client_address = SERVER.accept()
+        client, client_address = server.accept()
         print("%s:%s si è collegato." % client_address)
         Thread(target=gestisce_client, args=(client,)).start()
 
@@ -17,7 +17,7 @@ def gestisce_client(client):
 
         if "/" == filename.decode():
             filename = b'/index.html'
-        print('filename dopo =', filename.decode())
+        # print('filename dopo =', filename.decode())
 
         f = open(filename[1:],'rb') 
         outputdata = f.read()
@@ -34,20 +34,31 @@ def gestisce_client(client):
         client.send(bytes("HTTP/1.1 404 Not Found\r\n\r\n","UTF-8"))
         client.send(bytes("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n","UTF-8"))
         client.close()
+        
+def signal_handler(signal, frame):
+    print( 'Exiting server (Ctrl+C pressed)')
+    try:
+      if( server ):
+        server.server_close()
+    finally:
+      sys.exit(0)
 
+#interrompe l’esecuzione se da tastiera arriva la sequenza (CTRL + C) 
+signal.signal(signal.SIGINT, signal_handler)
 HOST = 'localhost'
 PORT = 8080
 BUFSIZ = 1024
 ADDR = (HOST, PORT)
 
-SERVER = socket(AF_INET, SOCK_STREAM)
-SERVER.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-SERVER.bind(ADDR)
+server = socket(AF_INET, SOCK_STREAM)
+server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+server.bind(ADDR)
 
 if __name__ == "__main__":
-    SERVER.listen(5)
+    server.listen(5)
+    print("Madacascar Web Server in esecuzione")
     print("In attesa di connessioni...")
-    ACCEPT_THREAD = Thread(target=accetta_richieste)
-    ACCEPT_THREAD.start()
-    ACCEPT_THREAD.join()
-    SERVER.close()
+    accept_thread = Thread(target=accetta_richieste)
+    accept_thread.start()
+    accept_thread.join()
+    server.close()
